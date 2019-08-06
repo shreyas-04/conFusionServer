f13d468f349f84042ae7ac38a8805f93fb0c9c6c
Shreyas Handiyekar <shreyashandiyekar45@gmail.com>
2019-08-06 09:00:51 +0530

    Express Session Part 2

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 
-const Dishes = require('./models/dishes');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var promoRouter = require('./routes/promoRouter');
+
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
@@ -40,39 +41,23 @@ app.use(session({
     store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
     console.log(req.session);
 
     if (!req.session.user) {
 
-        var authHeader = req.headers.authorization;
-        if (!authHeader) {
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
-
-        var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-
-        var username = auth[0];
-        var password = auth[1];
-
-        if (username == 'admin' && password == 'password') {
-            req.session.user = 'admin';
-            next();
-        } else {
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        return next(err);
     } else {
-        if (req.session.user == 'admin') {
+        if (req.session.user == 'authenticated') {
             next();
         } else {
             var err = new Error('You are not authenticated!');
-            err.status = 401;
+            err.status = 403;
             return next(err);
         }
     }
@@ -82,8 +67,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
 app.use('/promos', promoRouter);
