e2f84f2ea805db27cae0774824609482e1a57a4b
Shreyas Handiyekar <shreyashandiyekar45@gmail.com>
2019-08-07 18:42:43 +0530

    passport

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var logger = require('morgan');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const passport = require('passport');
 
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -41,25 +43,19 @@ app.use(session({
     store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-    console.log(req.session);
-
-    if (!req.session.user) {
-
+    if (!req.user) {
         var err = new Error('You are not authenticated!');
-        err.status = 401;
+        err.status = 403;
         return next(err);
     } else {
-        if (req.session.user == 'authenticated') {
-            next();
-        } else {
-            var err = new Error('You are not authenticated!');
-            err.status = 403;
-            return next(err);
-        }
+        next();
     }
 }
 
