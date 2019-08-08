089c644f588943fd62f5b7870e96b4349ce973d1
Shreyas Handiyekar <shreyashandiyekar45@gmail.com>
2019-08-08 21:18:39 +0530

    res.json(dish.comments);

--- models/user.js
+++ models/user.js
@@ -8,6 +8,16 @@ var User = new Schema({
     admin: {
         type: Boolean,
         default: false
+    },
+
+    firstname: {
+        type: String,
+        default: ''
+    },
+
+    lastname: {
+        type: String,
+        default: ''
     }
 });
 
