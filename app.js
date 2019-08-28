f495a941435d9f53b4bf759693cfabe55bda9bca
Shreyas Handiyekar <shreyashandiyekar45@gmail.com>
2019-08-29 00:57:55 +0530

    upload

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var promoRouter = require('./routes/promoRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 
 const url = config.mongoUrl;
@@ -57,6 +58,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
 app.use('/promos', promoRouter);
+app.use('/imageUpload', uploadRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
