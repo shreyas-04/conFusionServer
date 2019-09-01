0724589c63a7382dd3ee7bcff98b9ee04bc915ca
Shreyas Handiyekar <shreyashandiyekar45@gmail.com>
2019-09-01 15:06:33 +0530

    integrating client and server

--- routes/cors.js
+++ routes/cors.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 
 const app = express();
 
-const whiteList = ['http://localhost:3000', 'https://localhost:3443'];
+const whiteList = ['http://localhost:3000', 'https://localhost:3443', 'http://localhost:4200'];
 
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
